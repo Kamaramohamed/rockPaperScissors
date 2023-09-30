6e5d9c6b3fccdac77318135237ebd9ecbac216e6
Mohamed Kamara <info.mohamedkamara@gmail.Com>
2023-09-30 17:58:49 +0200

    eerste deel is klaar

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 let playerScore = 0;
-let computerScore = 0;
+let compScore = 0;
 
 
-
-function getComputerChoice() {
-
-    const choices = ["ROCK", "PAPER", "SCISSORS"];
-    const randomNum = Math.floor(Math.random() * 3)
-    const computerChoice = choices[randomNum]
-    return computerChoice
-    
-};
-
-const playRound  = (playerSelection, computerSelection) => {
-    if (playerSelection === computerSelection) {
-        return "tie";
-     }  if ( (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-     (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
-         (playerSelection === 'PAPER' && computerSelection === 'ROCK')) {
-         playerScore++
-         return "player win!"
-        
-     }  if (  (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
-     (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
-         (computerSelection === 'PAPER' && playerSelection === 'ROCK')) {
-         computerScore++
-        return"computer win!" 
-     }
+const computerPlay = () => {
+    const arrOfChoices = ["rock", "paper", "scissors"];
+    const randomNum = Math.floor(Math.random() * 3);
+    const compChoice = arrOfChoices[randomNum];
+    return compChoice;
 }
 
-
- 
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "You tied!"
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "You tied!"
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+    
+        return "You tied!"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        compScore++
+        return "You Lose!"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
+        return "You won!"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        compScore++
+        return "You Lose!"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
+        return "You won!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        compScore++
+        return "You Lose!"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
+        return "You Win!"
+    }
+}
 
 
 
@@ -38,79 +44,18 @@ const playRound  = (playerSelection, computerSelection) => {
 const game = () => {
     
     for (let i = 0; i < 5; i++){
-    const playerSelection = prompt('ROCK, PAPER or SCISSORS', 'vul in ').toLowerCase();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+        const playerSelection = prompt("Chose ROCK PAPER OR SCISSORS", "vul in").toLowerCase();
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
     }
 
-    if (playerScore > computerScore) {
-        return " you have beat the computer!"
-    } else if (computerScore > playerScore) {
-        return "computer has beat you!"
+    if (playerScore > compScore) {
+        return "You beat the computer!"
+    } else if (compScore > playerScore) {
+        return " you lose sorry!"
     } else {
         return "it was a tie!"
     }
 }
 
 console.log(game());
-
-//console.log(computerChoice())
-
-
-/* const getComputerChoice = () => {
-    let randomNumberComputer = Math.floor(Math.random() * 3)
-    
-   
-
-    switch (randomNumberComputer) {
-        case 0:
-            return "Rock"
-           
-        case 1:
-            return "Paper"
-           
-        case 2:
-            return "Scissors"
-        
-    
-            
-    };
-   
-  
-
-};
-
-
-const playerChoice = () => {
-    let randomNumberPlayer = Math.floor(Math.random() * 3)
-    
-   
-
-    switch (randomNumberPlayer) {
-        case 0:
-            return "ROCK"
-           
-        case 1:
-            return "PAPER"
-           
-        case 2:
-            return "SCISSORS"
-        
-    
-    };
-
-    
-
-};
-
-let playerSelection = playerChoice;
-let computerSelection = getComputerChoice;
-
-
-
-
-
-
-
-
- */
